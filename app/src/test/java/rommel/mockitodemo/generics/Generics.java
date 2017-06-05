ae1e73203c8c1b99a2b934d7ee40d1ca35a87cb6
yuanhaitao <492668570@qq.com>
2017-06-05 11:11:44 +0800

    增加泛型声明方法的说明

--- app/src/test/java/rommel/mockitodemo/generics/Generics.java
+++ app/src/test/java/rommel/mockitodemo/generics/Generics.java
@@ -5,8 +5,11 @@ package rommel.mockitodemo.generics;
  */
 
 public class Generics {
-    public static <T> T getObject(Class<T> c) throws IllegalAccessException, InstantiationException {
-        T instance = c.newInstance();
+    // (1)处的声明告诉编译器(2)(3)处的T是泛型。
+    // (3)处的声明告诉编译器(4)处的T的类型在我这里
+    // (3)决定了(4)(2)的类型
+    public static <T>/*(1)*/ T/*(2)*/ getObject(Class<T>/*(3)*/ c) throws IllegalAccessException, InstantiationException {
+        T/*(4)*/ instance = c.newInstance();
 
         return instance;
     }
